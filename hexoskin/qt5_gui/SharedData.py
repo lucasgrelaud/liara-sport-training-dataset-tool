80f5cb30951058570e9de5a8cb61c94dc31a04b1
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2018-06-05 02:32:15 +0200

    Finish the tag table

--- hexoskin/qt5_gui/SharedData.py
+++ hexoskin/qt5_gui/SharedData.py
@@ -6,17 +6,19 @@ from hexoskin.standardize.breathing import Breathing
 from hexoskin.standardize.cardiology import Cardiology
 from hexoskin.standardize.moves import Moves
 
+
 class SharedData(QObject):
     update_sync = pyqtSignal()
-    update_tags = pyqtSignal(str)
+    update_tags = pyqtSignal(str, str, str)
+    tags_updated = pyqtSignal(str, str, str)
 
     def __init__(self):
         QObject.__init__(self)
         self.video_path = None
-        self.video_sync = None
+        self.video_sync = 'HH:SS:MM:zzz'
 
         self.data_dir = None
-        self.data_sync = None
+        self.data_sync = 'HH:SS:MM:zzz'
 
         self.breathing = None
         self.cardiology = None
@@ -24,7 +26,22 @@ class SharedData(QObject):
 
         self.tags = dict()
 
+        self.update_tags.connect(self.update_tags_action)
+
+
     def init_data(self):
         self.breathing = Breathing(self.data_dir, QDir.tempPath())
         self.cardiology = Cardiology(self.data_dir, QDir.tempPath())
         self.moves = Moves(self.data_dir, QDir.tempPath())
+
+    def update_tags_action(self, action, timecode, tag):
+        if action == 'add':
+            tagExist = self.tags.get(timecode)
+            self.tags[timecode] = tag
+            if tagExist:
+                self.tags_updated.emit('update', timecode, tag)
+            else:
+                self.tags_updated.emit(action, timecode, tag)
+        elif action == 'delete':
+            del self.tags[timecode]
+            self.tags_updated.emit(action, timecode, tag)
--- hexoskin/qt5_gui/sync_feeds/VideoPlayerWidget.py
+++ hexoskin/qt5_gui/sync_feeds/VideoPlayerWidget.py
@@ -31,7 +31,7 @@ class VideoPlayerWidget(QWidget):
         super(VideoPlayerWidget, self).__init__(parent)
 
         # Create the video elements
-        self.media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
+        self.__media_player = QMediaPlayer(self, QMediaPlayer.VideoSurface)
         self.__video_item = QGraphicsVideoItem()
         self.__scene = QGraphicsScene(self)
         self.__graphic_view = QGraphicsView(self.__scene)
@@ -43,33 +43,31 @@ class VideoPlayerWidget(QWidget):
         # Create the video controls
         self.__rotate_left_button = QPushButton()
         self.__rotate_left_button.setText("Rotate left")
-        self.__rotate_left_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowBack))
-        self.__rotate_left_button.setToolTip("Rotate the video 90° to the left")
+        self.__rotate_left_button.setToolTip("Rotate the video 90° to the left.")
         self.__rotate_left_button.setEnabled(False)
         self.__rotate_left_button.clicked.connect(self.__rotate_video_left)
 
         self.__rotate_right_button = QPushButton()
         self.__rotate_right_button.setText("Rotate right")
-        self.__rotate_right_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowForward))
-        self.__rotate_right_button.setToolTip("Rotate the video 90° to the right")
+        self.__rotate_right_button.setToolTip("Rotate the video 90° to the right.")
         self.__rotate_right_button.setEnabled(False)
         self.__rotate_right_button.clicked.connect(self.__rotate_video_right)
 
         self.__open_video_button = QPushButton()
         self.__open_video_button.setText("Open video")
         self.__open_video_button.setIcon(self.style().standardIcon(QStyle.SP_DriveCDIcon))
-        self.__open_video_button.setToolTip("Select and load the video clip")
+        self.__open_video_button.setToolTip("Select and load the video clip.")
         self.__open_video_button.clicked.connect(self.__open_file)
 
         self.__play_button = QPushButton()
         self.__play_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-        self.__play_button.setToolTip("Play the video")
+        self.__play_button.setToolTip("Play the video.")
         self.__play_button.setEnabled(False)
         self.__play_button.clicked.connect(self.__play)
 
         self.__stop_button = QPushButton()
         self.__stop_button.setIcon(self.style().standardIcon(QStyle.SP_MediaStop))
-        self.__stop_button.setToolTip("Stop the video")
+        self.__stop_button.setToolTip("Stop the video.")
         self.__stop_button.setEnabled(False)
         self.__stop_button.clicked.connect(self.__stop)
 
@@ -77,8 +75,8 @@ class VideoPlayerWidget(QWidget):
         self.__video_slider.setRange(0, 0)
         self.__video_slider.sliderMoved.connect(self.__set_position)
 
-        self.time_code = QLabel()
-        self.time_code.setText('HH:SS:MM:zzz / HH:SS:MM:zzz')
+        self.__time_code = QLabel()
+        self.__time_code.setText('HH:SS:MM:zzz / HH:SS:MM:zzz')
 
         # Create the layouts
         top_control_layout = QHBoxLayout()
@@ -93,7 +91,7 @@ class VideoPlayerWidget(QWidget):
         bottom_control_layout.addWidget(self.__play_button)
         bottom_control_layout.addWidget(self.__stop_button)
         bottom_control_layout.addWidget(self.__video_slider)
-        bottom_control_layout.addWidget(self.time_code)
+        bottom_control_layout.addWidget(self.__time_code)
 
         widget_layout = QVBoxLayout()
         widget_layout.addLayout(top_control_layout)
@@ -101,10 +99,11 @@ class VideoPlayerWidget(QWidget):
         widget_layout.addLayout(bottom_control_layout)
 
         # Configure the video player
-        self.media_player.setVideoOutput(self.__video_item)
-        self.media_player.stateChanged.connect(self.__media_state_changed)
-        self.media_player.positionChanged.connect(self.__position_changed)
-        self.media_player.durationChanged.connect(self.__duration_changed)
+        self.__media_player.setVideoOutput(self.__video_item)
+        self.__media_player.stateChanged.connect(self.__media_state_changed)
+        self.__media_player.positionChanged.connect(self.__position_changed)
+        self.__media_player.durationChanged.connect(self.__duration_changed)
+        self.__media_player.error.connect(self.handle_error)
 
         # Set the widget layout
         self.setLayout(widget_layout)
@@ -119,32 +118,33 @@ class VideoPlayerWidget(QWidget):
             self.__load_video(file_dialog.selectedUrls()[0])
 
     def __load_video(self, url):
-        self.media_player.setMedia(QMediaContent(url))
+        self.__media_player.setMedia(QMediaContent(url))
         self.__play_button.setEnabled(True)
         self.__stop_button.setEnabled(True)
         self.__rotate_left_button.setEnabled(True)
         self.__rotate_right_button.setEnabled(True)
         self.__update_time_code(0)
+        self.file_loaded.emit()
 
     def __play(self):
-        if self.media_player.state() == QMediaPlayer.PlayingState:
-            self.media_player.pause()
+        if self.__media_player.state() == QMediaPlayer.PlayingState:
+            self.__media_player.pause()
         else:
-            self.media_player.play()
+            self.__media_player.play()
 
     def __stop(self):
-        self.media_player.stop()
+        self.__media_player.stop()
 
     def __media_state_changed(self, state):
         if state == QMediaPlayer.PlayingState:
             self.__play_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
-            self.__play_button.setToolTip("Pause the video")
+            self.__play_button.setToolTip("Pause the video.")
         else:
             self.__play_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
-            self.__play_button.setToolTip("Play the video")
+            self.__play_button.setToolTip("Play the video.")
 
     def __set_position(self, position):
-        self.media_player.setPosition(position)
+        self.__media_player.setPosition(position)
 
     def __position_changed(self, position):
         self.__video_slider.setValue(position)
@@ -158,14 +158,14 @@ class VideoPlayerWidget(QWidget):
     def __update_time_code(self, current_position):
         time_string = ''
         current_position /= 1000
-        if current_position and self.media_player.duration():
-            duration = self.media_player.duration() / 1000
+        if current_position and self.__media_player.duration():
+            duration = self.__media_player.duration() / 1000
             current_time = QTime(int((current_position / 3600) % 60), int((current_position / 60) % 60),
                                  int(current_position % 60), int((current_position * 1000) % 1000))
 
             time_string = '{} / {}'.format(current_time.toString("HH:mm:ss:zzz"),
                                            self.__video_duration.toString("HH:mm:ss:zzz"))
-            self.time_code.setText(time_string)
+            self.__time_code.setText(time_string)
 
     def __rotate_video_left(self):
         if self.__rotation != 270:
@@ -186,3 +186,9 @@ class VideoPlayerWidget(QWidget):
         x = self.__video_item.boundingRect().width() / 2
         y = self.__video_item.boundingRect().height() / 2
         self.__video_item.setTransform(QTransform().translate(x, y).rotate(self.__rotation).translate(-x, -y))
+
+    def handle_error(self, error):
+        self.error.emit(error)
+
+    def current_timecode(self):
+        return self.__time_code.text().split(' ')[0]
